94d6252b114a1c9c8a71709b6002f0bddee08d00
RAO <raorao1011@gmail.com>
2021-03-05 22:24:56 +0900

    new calc

---
+++ src/redux/actions/types.jsx
@@ -0,0 +1,14 @@
+// 数字がクリックされたとき
+export const INPUT_NUMBER = "INPUT_NUMBER";
+
+// +,-,x,÷がクリックされたとき
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const MULTIPLY = "MULTTTIPLY";
+export const DIVIDE = "DIVIDE";
+
+// = がクリックされたとき
+export const EQUAL = "EQUAL";
+
+// ACがクリックされたとき
+export const CLEAR = "CLEAR";
